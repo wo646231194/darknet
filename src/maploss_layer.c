0ed68db32bafca7d1c0228ff600022c639b99e1a
wo646231194 <wo646231194@live.com>
2016-11-09 22:32:22 +0800

    2016-11-09 maploos add step and size(roi)

--- src/maploss_layer.c
+++ src/maploss_layer.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-maploss_layer make_maploss_layer(int batch, int inputs, int classes, int coords, int size, int num, int h, int w)
+maploss_layer make_maploss_layer(int batch, int inputs, int classes, int coords, int size, int num, int h, int w, int step)
 {
     maploss_layer l = {0};
     l.type = MAPLOSS;
@@ -22,21 +22,31 @@ maploss_layer make_maploss_layer(int batch, int inputs, int classes, int coords,
     l.size = size;
     l.w = w;
     l.h = h;
+    l.steps = step;
     fprintf(stderr, "MapLoss Layer : %d scale\n", num);
     assert(num*(classes+coords) == inputs);
     l.n = num;
     l.cost = calloc(1, sizeof(float));
     l.outputs = l.inputs*l.w*l.h;
     l.truths = 50*(5+l.classes);//if 50 person one image
-    l.output = calloc(batch*l.outputs, sizeof(float));
+    if(l.size>0){
+        l.output = calloc(batch*l.outputs, sizeof(float));
+    }else{
+        l.output = calloc(batch*l.inputs, sizeof(float));
+    }
     l.delta = calloc(batch*l.inputs, sizeof(float));
+    l.indexes = calloc(batch*l.n, sizeof(int));
 
     // l.forward = forward_maploss_layer;
     l.backward = backward_maploss_layer;
 #ifdef GPU
     // l.forward_gpu = forward_maploss_layer_gpu;
     l.backward_gpu = backward_maploss_layer_gpu;
-    l.output_gpu = cuda_make_array(l.output, batch*l.outputs);
+    if(l.size>0){
+        l.output_gpu = cuda_make_array(l.output, batch*l.outputs);
+    }else{
+        l.output_gpu = cuda_make_array(l.output, batch*l.inputs);
+    }
     l.delta_gpu = cuda_make_array(l.delta, batch*l.inputs);
 #endif
 
@@ -56,8 +66,53 @@ void forward_maploss_layer(const maploss_layer l, network_state state, int n, in
     int count = 0;
     int size = l.inputs * l.batch;
     memset(l.delta, 0, size * sizeof(float));
-    //----------------neg---------------
+    memset(l.indexes, 0, l.batch * l.n * sizeof(int));
     ACTIVATION active = get_activation("logistic");
+    if(l.size==0){
+        float siou = 0.0;
+        for (i = 0; i < l.batch; ++i){
+            for (j = 0; j < l.n; ++j){
+                int in = (i*l.n + j)*(l.coords + l.classes);
+                float h_theta_y[1] = { state.input[in] };
+                activate_array(h_theta_y, 1, active);
+
+                box out = float_to_box(state.input + in + 1);
+                out.x = (out.x + x[i]) / l.w;
+                out.y = (out.y + y[i]) / l.h;
+                out.h = out.h*out.h;
+                out.w = out.h * 0.41 / 64 * 48;
+
+                l.output[in + 0] = h_theta_y[0];
+                l.output[in + 1] = out.x-out.w/2;//left
+                l.output[in + 2] = out.y-out.h/2;//top
+                l.output[in + 3] = out.w+out.w/2;//right
+                l.output[in + 4] = out.h+out.h/2;//bottom
+
+                int truth_index = i * 50 * (5 + l.classes),t=0;
+                while(state.truth[truth_index]==1){
+                    box truth;
+                    truth = float_to_box(state.truth + truth_index + 1 + l.classes);
+                    
+                    truth.x /= width;
+                    truth.y /= height;
+                    out.x /= width;
+                    out.y /= height;
+                    float tiou = box_iou(truth, out);
+                    if(tiou>0.7){
+                        siou += tiou;
+                        count++;
+                        l.indexes[i*l.n + j] = 1;//----pos----
+                    }
+                    t++;
+                    truth_index += t*(5 + l.classes);
+                }
+            }
+        }
+        if(count)
+        printf("ROILoss Avg IOU: %f, ", siou / count);
+        return;
+    }
+    //----------------neg---------------
     for (i = 0; i < l.batch; ++i){
         for (j = 0; j < l.n; ++j){
             int in = (i*l.n + j)*(l.coords + l.classes);
@@ -85,7 +140,6 @@ void forward_maploss_layer(const maploss_layer l, network_state state, int n, in
             truth_index += t*(5 + l.classes);
         }
         if (is_obj==0) continue;
-        count++;
 
         truth.x = truth.x*width - x[i];
         truth.y = truth.y*height - y[i];
@@ -102,6 +156,11 @@ void forward_maploss_layer(const maploss_layer l, network_state state, int n, in
             out.h = out.h*out.h;
             out.w = out.h * 0.41 / 64 * 48;
 
+            truth.x /= width;
+            truth.y /= height;
+            out.x /= width;
+            out.y /= height;
+
             float tiou  = box_iou(out, truth);
             float rmse = box_rmse(out, truth);
             if(best_iou > 0 || tiou > 0){
@@ -115,44 +174,77 @@ void forward_maploss_layer(const maploss_layer l, network_state state, int n, in
                     best_index = j;
                 }
             }
+            if(l.steps == 1 && tiou > 0.5){
+                count++;
+                // int p_index = i*l.inputs + j*(l.coords + l.classes);
+                //---------------fix box---------------
+                // box out = float_to_box(state.input + p_index + 1);
+
+                out.h = out.h*out.h;
+                out.w = out.h * 0.41 / 64 * 48;
+
+                float coord[4] = { out.x , out.y , 0, state.input[p_index+4] };
+                float tcoord[4] = { truth.x, truth.y, 0, sqrt(truth.h) };
+                float dcoord[4] = { 0 };
+                float ecoord[4] = { 0 };
+
+                //----------------loc loss--------------------
+                l2_cpu(4, coord, tcoord, dcoord, ecoord);
+                l.delta[p_index + 1] = dcoord[0]*l.coord_scale;
+                l.delta[p_index + 2] = dcoord[1]*l.coord_scale;
+                l.delta[p_index + 3] = dcoord[2]*l.coord_scale;
+                l.delta[p_index + 4] = dcoord[3]*l.coord_scale;
+                loc_loss += sum_array(ecoord, 4);
+
+                //-----------------pos loss--------------------
+                float h_theta_y[1] = { state.input[p_index] };
+                activate_array(h_theta_y, 1, active);
+                float delta[1] = { (1.0 - h_theta_y[0]) };
+                gradient_array(h_theta_y, 1, active, delta);
+                l.delta[p_index] = delta[0]*l.object_scale;
+                pos_loss -= log(h_theta_y[0]);
+                
+                iou += tiou;
+            }
         }
+        if(best_iou > 0.5){
 
-        int p_index = i*l.inputs + best_index*(l.coords + l.classes);
-        //---------------fix box---------------
-        box out = float_to_box(state.input + p_index + 1);
+            count++;
+            int p_index = i*l.inputs + best_index*(l.coords + l.classes);
+            //---------------fix box---------------
+            box out = float_to_box(state.input + p_index + 1);
 
-        out.h = out.h*out.h;
-        out.w = out.h * 0.41 / 64 * 48;
+            out.h = out.h*out.h;
+            out.w = out.h * 0.41 / 64 * 48;
 
-        float coord[4] = { out.x , out.y , 0, state.input[p_index+4] };
-        float tcoord[4] = { truth.x, truth.y, 0, sqrt(truth.h) };
-        float dcoord[4] = { 0 };
-        float ecoord[4] = { 0 };
+            float coord[4] = { out.x , out.y , 0, state.input[p_index+4] };
+            float tcoord[4] = { truth.x, truth.y, 0, sqrt(truth.h) };
+            float dcoord[4] = { 0 };
+            float ecoord[4] = { 0 };
 
-        //----------------loc loss--------------------
-        l2_cpu(4, coord, tcoord, dcoord, ecoord);
-        l.delta[p_index + 1] = dcoord[0]*l.coord_scale;
-        l.delta[p_index + 2] = dcoord[1]*l.coord_scale;
-        l.delta[p_index + 3] = dcoord[2]*l.coord_scale;
-        l.delta[p_index + 4] = dcoord[3]*l.coord_scale;
-        loc_loss += sum_array(ecoord, 4);
+            //----------------loc loss--------------------
+            l2_cpu(4, coord, tcoord, dcoord, ecoord);
+            l.delta[p_index + 1] = dcoord[0]*l.coord_scale;
+            l.delta[p_index + 2] = dcoord[1]*l.coord_scale;
+            l.delta[p_index + 3] = dcoord[2]*l.coord_scale;
+            l.delta[p_index + 4] = dcoord[3]*l.coord_scale;
+            loc_loss += sum_array(ecoord, 4);
 
-        //-----------------pos loss--------------------
-        float h_theta_y[1] = { state.input[p_index] };
-        activate_array(h_theta_y, 1, active);
-        float delta[1] = { (1.0 - h_theta_y[0]) };
-        gradient_array(h_theta_y, 1, active, delta);
-        l.delta[p_index] = delta[0]*l.object_scale;
-        pos_loss -= log(h_theta_y[0]);
+            //-----------------pos loss--------------------
+            float h_theta_y[1] = { state.input[p_index] };
+            activate_array(h_theta_y, 1, active);
+            float delta[1] = { (1.0 - h_theta_y[0]) };
+            gradient_array(h_theta_y, 1, active, delta);
+            l.delta[p_index] = delta[0]*l.object_scale;
+            pos_loss -= log(h_theta_y[0]);
 
-        truth.x /= width;
-        truth.y /= height;
-        out.x /= width;
-        out.y /= height;
-        
-        iou += box_iou(truth, out);
-
-        // printf("%f,%f,%f,%f,%f",l.delta[p_index],l.delta[p_index+1],l.delta[p_index+2],l.delta[p_index+3],l.delta[p_index+4]);
+            truth.x /= width;
+            truth.y /= height;
+            out.x /= width;
+            out.y /= height;
+            
+            iou += box_iou(truth, out);
+        }
     }
     *(l.cost) += pos_loss + neg_loss/l.n + loc_loss;
 
@@ -228,6 +320,7 @@ void forward_maploss_layer_gpu(const maploss_layer l, network_state state, int n
     cpu_state.truth = truth_cpu;
     cpu_state.input = in_cpu;
     forward_maploss_layer(l, cpu_state, n, height, width, x, y);
+    cuda_push_array(l.output_gpu, l.output, l.batch*l.inputs);
     cuda_push_array(l.delta_gpu, l.delta, l.batch*l.inputs);
     free(cpu_state.input);
     if(cpu_state.truth) free(cpu_state.truth);//---------------------------truth_cpu end
